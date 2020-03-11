fd173a6ac3fdb776ef5ac96992af5a8d8e6e0f24
Doug Kellermeyer <dkellermeyer@jorsek.com>
2020-03-11 12:50:03 -0400

    added BDD using describe blocks

--- giftgiver/src/components/App.js
+++ giftgiver/src/components/App.js
@@ -25,7 +25,16 @@ class App extends Component {
         return (
             <div>
                 <h2>Gift Giver</h2>
-                <Button className='btn-add' onClick={this.addGift}>Add Gift</Button>
+                <div className='gift-list'>
+                    {
+                        this.state.gifts.map(gift => {
+                            return (
+                                <div key={gift.id}></div>
+                            )
+                        })
+                    }
+                </div>
+                <Button style={{backgroundColor: "lightblue"}} className='btn-add' onClick={this.addGift}>Add Gift</Button>
             </div>
         )
     }
--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -5,15 +5,33 @@ import App from './App';
 
 const app = shallow(<App />);
 
-it('renders correctly', () => {
-    expect(app).toMatchSnapshot();
+//App unit tests
+describe('App', () => {
+    it('renders correctly', () => {
+        expect(app).toMatchSnapshot();
+    });
+    
+    it('initializes the `state` with an empty list of gifts', () => {
+        expect(app.state().gifts).toEqual([]);
+    });
+
+    describe('when clicking the add gift button', () => {
+        //use a beforeEach to click the button before executing this describe block
+        beforeEach(() => {
+            app.find('.btn-add').simulate('click');
+        });
+        //cleaning up the button click, reset the gift-list array after each test, so we avoid test pollution
+        afterEach(() => {
+            app.setState({gifts: []})
+        });
+        it('adds a new gift to `state` when clicking the `add gift` button', () => {
+            expect(app.state().gifts).toEqual([{id: 1}])
+        });
+        
+        it('adds a new gift to the rendered list when clicking the add gift button', () => {            
+            expect(app.find('.gift-list').children().length).toEqual(1);
+        });
+    });
 });
 
-it('initializes the `state` with an empty list of gifts', () => {
-    expect(app.state().gifts).toEqual([]);
-});
 
-it('addes a new gift to `state` when clicking the `add gift` button', () => {
-    app.find('.btn-add').simulate('click');
-    expect(app.state().gifts).toEqual([{id: 1}])
-});
