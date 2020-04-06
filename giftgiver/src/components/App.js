ecf5756e356e3f8ce323d0a3b982d0ce6f24d854
Doug Kellermeyer <dkellermeyer@jorsek.com>
2020-04-06 18:21:08 -0400

    improved code coverage and added styles

--- giftgiver/src/components/App.js
+++ giftgiver/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
+import { max_number } from '../helper';
 import Gift from './Gift';
 
 class App extends Component {
@@ -14,10 +15,7 @@ class App extends Component {
     addGift = () => {
         const { gifts } = this.state;
 
-        const ids = this.state.gifts.map(gift => gift.id);
-        const max_id =  ids.length > 0 ? Math.max(...ids) : 0;
-
-        gifts.push({id: max_id+1});
+        gifts.push({id: max_number(this.state.gifts.map(gift => gift.id))+1});
 
         this.setState({ gifts });
     };
@@ -46,7 +44,7 @@ class App extends Component {
                         })
                     }
                 </div>
-                <Button style={{backgroundColor: "lightblue"}} className='btn-add' onClick={this.addGift}>Add Gift</Button>
+                <Button className='btn-add' onClick={this.addGift}>Add Gift</Button>
             </div>
         )
     }
